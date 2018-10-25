36d81e48cd9cc52c533dd0939742078c6e813975
Jonathan Smith <jonathan@peoplesized.com>
2018-10-25 15:19:17 +0200

    basic list of algos

---
+++ src/components/ListAlgos.vue
@@ -0,0 +1,32 @@
+<template>
+  <div>
+    <h2>Sample Models Menu for Reveal team</h2>
+
+    <ul>
+      <li v-for="algo in algos" :key="algo.id">
+        {{algo.name}} ({{algo.version}}) <a :href="algo.docs_link">docs</a> - {{algo.description}}
+      </li>
+    </ul>
+  </div>
+</template>
+
+<script>
+  import axios from 'axios'
+
+  export default {
+    name: 'ListAlgos',
+    data() {
+      return {
+        algos: []
+      }
+    },
+    async mounted() {
+      const res = await axios('http://localhost:1337/algos')
+      this.algos = res.data
+    }
+  }
+</script>
+
+<style scoped>
+
+</style>
